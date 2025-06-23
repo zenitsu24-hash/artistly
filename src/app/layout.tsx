879a002eae2ba2d611041af515d707472376b6cb
harsh <harsh2401@gmaail.com>
2025-06-24 02:01:20 +0530

    m

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,13 +29,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
-        <ThemeProvider attribute="class"
-            defaultTheme="white"
-            enableSystem
-            disableTransitionOnChange>
           <Navbar/>
           {children}
-        </ThemeProvider>
       </body>
     </html>
   );
